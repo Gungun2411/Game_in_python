560cceba2e0f645a53d38702d79127bbcc4cbec6
GUNGUN SAINI <sainigungun856@gmail.com>
2026-02-14 16:38:05 +0530

    Add files via upload

---
+++ Game.py
@@ -0,0 +1,48 @@
+import random
+
+def game_win(user,computer):
+    if user == computer:
+        return None
+    
+    if user == "s" and computer == "w":
+        return True 
+    if user == "w" and computer == "s":
+        return False 
+
+
+
+    if user == "w" and computer == "g":
+        return True 
+    if user == "g" and computer == "w":
+        return False 
+
+
+    if user == "g" and computer == "s":
+        return True 
+    if user == "s" and computer == "g":
+        return False 
+
+
+rand_no = random.randint(1,3)
+
+print("Computer's turn: snake(s), water(w),gun(g)")
+if rand_no == 1:
+       computer="s"
+elif rand_no== 2:
+       computer ="w"    
+else:
+       computer="g"        
+
+
+user =input("your turn: snake(s), water(w),gun(g):").lower()
+result= game_win(user,computer)
+print(f"\nYou choose :{user}")
+print(f"\nComputer choose: {computer}")
+
+if result is None:
+       print("its a draw")
+elif(result):
+       print ("you win")
+else:
+       print("you loose")
+
